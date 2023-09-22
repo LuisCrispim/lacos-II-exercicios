64bdcd63a637ae966e5de64c06d62dd905de8b2a
unknown <fcrispim2012@gmail.com>
2023-09-22 18:08:58 -0300

    Exercícios Prontos

---
+++ exercicio2/script.js
@@ -0,0 +1,16 @@
+
+
+
+// const tabPergunta = prompt("Informe um número para a tabuada")
+
+// for (let tabuada = 1; tabuada <= 10; tabuada++) {
+//     console.log(tabPergunta * tabuada)
+// }
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const j = prompt("Informe um número para a tabuada")
+for (let i in numeros) {
+    
+    console.log(j * numeros[i])
+}
+
